a15cec6d0076802cd735f146f70596ff9a5d0399
Erick Lino <eri.sion@live.com>
2023-07-18 20:07:55 -0500

    Solución a la data necesaria para los PUT

--- users/serializer.py
+++ users/serializer.py
@@ -4,3 +4,8 @@ class MemberSerializer(serializers.ModelSerializer):
     class Meta:
         model= Member
         fields = '__all__'
+        read_only_fields = ['username','password']
+        
+    def update(self, instance, validated_data):
+        print(instance.favcite.id)
+        return super().update(instance, validated_data)
